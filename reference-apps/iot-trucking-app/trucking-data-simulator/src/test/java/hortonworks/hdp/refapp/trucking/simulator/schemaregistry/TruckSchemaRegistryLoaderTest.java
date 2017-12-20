4d4cae9b43005e59f370762d7f0cbb74297aad8f
georgevetticaden <george.vetticaden@gmail.com>
2017-12-20 11:34:33 -0600

    upgradign to latest SR client

--- reference-apps/iot-trucking-app/trucking-data-simulator/src/test/java/hortonworks/hdp/refapp/trucking/simulator/schemaregistry/TruckSchemaRegistryLoaderTest.java
+++ reference-apps/iot-trucking-app/trucking-data-simulator/src/test/java/hortonworks/hdp/refapp/trucking/simulator/schemaregistry/TruckSchemaRegistryLoaderTest.java
@@ -35,8 +35,8 @@ import com.hortonworks.registries.schemaregistry.serdes.avro.AvroSnapshotSeriali
 
 public class TruckSchemaRegistryLoaderTest {
 
-	private static final String SCHEMA_REGISTRY_URL = "http://ctr-e134-1499953498516-283555-01-000002.hwx.site:7788/api/v1";
-	//private static final String SCHEMA_REGISTRY_URL = "http://hdf-3-1-build3.field.hortonworks.com:7788/api/v1";
+	//private static final String SCHEMA_REGISTRY_URL = "http://ctr-e134-1499953498516-283555-01-000002.hwx.site:7788/api/v1";
+	private static final String SCHEMA_REGISTRY_URL = "http://hdf-3-1-build3.field.hortonworks.com:7788/api/v1";
 	//private static final String SCHEMA_REGISTRY_URL = "http://hdf-3-0-1-maint2.field.hortonworks.com:7788/api/v1";
 	
 	protected Logger LOG = Logger.getLogger(TruckSchemaRegistryLoader.class.getName());
